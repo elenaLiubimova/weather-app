1d3bfe09ac5fb485a40c25e447f473c6fe8bb94c
Elena Liubimova <elena.liubimova3@gmail.com>
2023-01-20 22:17:55 +0300

    feat: сверстать блок текущей погоды

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Home />
     </div>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import CurrentWeather from '../../components/CurrentWeather/CurrentWeather';
+import Header from '../../components/Header/Header';
 import styles from './Home.module.scss';
 
 const Home = () => {
   return (
-    <main>
-      
-    </main>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <CurrentWeather />
+      </main>
+    </>
   );
-}
+};
 
 export default Home;
