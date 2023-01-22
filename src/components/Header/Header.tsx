8cd3083005415375e95dd0091bc56aadaf3c5395
Elena Liubimova <elena.liubimova3@gmail.com>
2023-01-22 22:57:07 +0300

    feat: реализовать простую смену темы

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import logo from "../../img/logo.png";
-import sun from "../../img/sun.svg";
-import moon from "../../img/moon.svg";
+import logo from '../../img/logo.png';
+import sun from '../../img/sun.svg';
+// import moon from '../../img/moon.svg';
 // import search from "../../img/search.svg";
 
 const Header = () => {
+  const [theme, setTheme] = React.useState('light');
+
+  function toggleTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+
+  React.useEffect(() => {
+    const root = document.querySelector(':root') as HTMLElement;
+    root.style.setProperty(
+      '--background-color',
+      `var(--background-color-theme-${theme})`
+    );
+    root.style.setProperty(
+      '--font-color',
+      `var(--font-color-theme-${theme})`
+    );
+  }, [theme]);
+
   return (
     <header>
       <div className={styles.logoAndTitle}>
-        <img className={styles.logo} src={logo} alt="логотип приложения: солнце с облаком"/>
+        <img
+          className={styles.logo}
+          src={logo}
+          alt="логотип приложения: солнце с облаком"
+        />
         <p className={styles.title}>Погода</p>
         {/* <button className={styles.searchButton}/> */}
-        <input placeholder='Населенный пункт'/>
+        <input placeholder="Населенный пункт" />
       </div>
       <div className={styles.toggles}>
         <img src={sun} />
-        <button className={styles.themeToggle}>
+        <button className={styles.themeToggle} onClick={toggleTheme}>
           <div />
         </button>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
