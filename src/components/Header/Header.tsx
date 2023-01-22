8fe0c662bc550dc70410f0072df07698b9a2cb08
Elena Liubimova <elena.liubimova3@gmail.com>
2023-01-22 19:59:55 +0300

    feat: создать кнопку переключения темы

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import logo from "../../img/logo.png";
-import search from "../../img/search.svg";
+import sun from "../../img/sun.svg";
+import moon from "../../img/moon.svg";
+// import search from "../../img/search.svg";
 
 const Header = () => {
   return (
     <header>
       <div className={styles.logoAndTitle}>
-        <img className={styles.logo} src={logo}/>
+        <img className={styles.logo} src={logo} alt="логотип приложения: солнце с облаком"/>
         <p className={styles.title}>Погода</p>
-      </div>
-      <div className={styles.searchBlock}>
-        <button className={styles.searchButton}/>
+        {/* <button className={styles.searchButton}/> */}
         <input placeholder='Населенный пункт'/>
+      </div>
+      <div className={styles.toggles}>
+        <img src={sun} />
+        <button className={styles.themeToggle}>
+          <div />
+        </button>
       </div>
     </header>
   );
